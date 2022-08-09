344e9c6a5a6d75916e0db19f082d9d189f9d2c16
LucienKalala <murhulakalalalucien14@gmail.com>
2022-08-09 16:09:11 +0200

    first commit

---
+++ assets/js/bs-init.js
@@ -0,0 +1,16 @@
+
+if (window.innerWidth < 768) {
+	[].slice.call(document.querySelectorAll('[data-bss-disabled-mobile]')).forEach(function (elem) {
+		elem.classList.remove('animated');
+		elem.removeAttribute('data-bss-hover-animate');
+		elem.removeAttribute('data-aos');
+	});
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+
+	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bss-tooltip]'));
+	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+	  return new bootstrap.Tooltip(tooltipTriggerEl);
+	})
+}, false);
